3e924e481a93207a95c5db9202cdbc9a3ab2f986
Ryan Le <ryanphle@gmail.com>
2017-08-20 18:42:40 -0700

    website pages change :)

--- mysite/mealplanner/views.py
+++ mysite/mealplanner/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
 #Home page for the landing page
+@csrf_exempt
 def home(request):
 	return render_to_response('homepage.html')
 
+@csrf_exempt
 def submit(request):
 	return render_to_response('test.html')

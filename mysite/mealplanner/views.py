335d4926385d91ffc09467d686d298852cb75530
Ryan Le <ryanphle@gmail.com>
2017-08-21 14:25:37 -0700

    Modified visibilities of table

--- mysite/mealplanner/views.py
+++ mysite/mealplanner/views.py
@@ -10,19 +10,21 @@ from .calorie_parser import *
 fullMenu = {}
 foodList = {}
 
-diningHallList = ['64 Degrees', 'Canyon Vista', 'OceanView Terrace', 'Cafe Ventanas', 'Foodworx', 'Pines']
+diningHallList = ['64 Degrees', 'Canyon Vista', 'OceanView Terrace', 
+'Cafe Ventanas', 'Foodworx', 'Pines']
 
-# Error/Failure Messages
+# Message Strings
 reqDiningHallMsg = "*** A Dining Hall is required ***"
 reqCaloriesMsg = "*** Calories are required ***"
 noResultsMsg = "Sorry, there were no items at %s that are under %d calories."
+resultsMsg = "Here are the menu items at %s that are under %d calories."
 
 @csrf_exempt
 def home(request):
 	"""
 	Returns homepage as the landing page
 	"""
-	return render_to_response('homepage.html')
+	return render_to_response('homepage.html', {'tableVisiblity':"display: none;"})
 
 @csrf_exempt
 def submit(request):
@@ -35,11 +37,14 @@ def submit(request):
 
 		# Checking if dining hall and calories is entered, return required message if not
 		if dininghall not in diningHallList and calories == "":
-			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg, 'reqCalories':reqCaloriesMsg})
+			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg, 
+				'reqCalories':reqCaloriesMsg, 'tableVisiblity':"display: none;"})
 		elif dininghall not in diningHallList:
-			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg})
+			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg, 
+				'tableVisiblity':"display: none;"})
 		elif calories == "":
-			return render_to_response('homepage.html', {'reqCalories':reqCaloriesMsg})
+			return render_to_response('homepage.html', {'reqCalories':reqCaloriesMsg, 
+				'tableVisiblity':"display: none;"})
 
 		calories = int(calories)
 		fullMenu = populateList(dininghall)
@@ -47,9 +52,12 @@ def submit(request):
 
 		# Checking if there were foods under the calorie limit
 		if (len(foodList) > 0):
-			return render_to_response('homepage.html', {'d':foodList})
+			return render_to_response('homepage.html', 
+				{'d':foodList, 'resultsMsg':resultsMsg%(dininghall, calories), 
+				'tableVisiblity':"display: visible;"})
 		else:
-			return render_to_response('homepage.html', {'noresults':noResultsMsg%(dininghall, calories)})
+			return render_to_response('homepage.html', 
+				{'noresults':noResultsMsg%(dininghall, calories)})
 
 	# Redirecting user to home page if went to link directly
 	else:

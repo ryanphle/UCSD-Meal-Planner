8753f84ce572a37ca4ba44d9ae4455a8c775f925
ryan <ryanle@Ryans-MacBook-Pro.local>
2017-08-20 17:27:44 -0700

    misc. changes w/ site fix

--- mysite/mealplanner/views.py
+++ mysite/mealplanner/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 #Home page for the landing page
 def home(request):
 	return render_to_response('homepage.html')
+
+def submit(request):
+	return render_to_response('test.html')

f07b9b2649fde2a304b67d2fc47151209601b7e5
Ryan Le <ryanphle@gmail.com>
2017-08-22 18:16:08 -0700

    Added check for empty menus for speed boost

--- mysite/mealplanner/views.py
+++ mysite/mealplanner/views.py
@@ -38,16 +38,23 @@ def submit(request):
 		# Checking if dining hall and calories is entered, return required message if not
 		if dininghall not in diningHallList and calories == "":
 			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg, 
-				'reqCalories':reqCaloriesMsg, 'tableVisiblity':"display: none;"})
+				'errCalories':reqCaloriesMsg, 'tableVisiblity':"display: none;"})
 		elif dininghall not in diningHallList:
 			return render_to_response('homepage.html', {'reqDiningHall':reqDiningHallMsg, 
 				'tableVisiblity':"display: none;", 'prevCalories':calories})
 		elif calories == "":
-			return render_to_response('homepage.html', {'reqCalories':reqCaloriesMsg, 
+			return render_to_response('homepage.html', {'errCalories':reqCaloriesMsg, 
 				'tableVisiblity':"display: none;", 'selectedHall':dininghall})
 
 		calories = int(calories)
 		fullMenu = populateList(dininghall)
+
+		# Checking if there are any items on the menu in the first place
+		if (len(fullMenu) == 0):
+			return render_to_response('homepage.html', 
+				{'noresults':noResultsMsg%(dininghall, calories), 
+				'tableVisiblity':"display: none;"})
+
 		foodList = findFoods(fullMenu, calories)
 
 		# Checking if there were foods under the calorie limit

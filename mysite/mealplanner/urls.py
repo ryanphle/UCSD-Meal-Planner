270a0e38b74e8b67c89bd3f67ff4d89c38b05c20
Ryan Le <ryanphle@gmail.com>
2017-08-25 19:38:00 -0700

    Redirect all incorrect sub-URLS to home 2

--- mysite/mealplanner/urls.py
+++ mysite/mealplanner/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^submit/', views.submit, name='submit'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
+    url(r'^.*$', views.submit, name='else')
 ]

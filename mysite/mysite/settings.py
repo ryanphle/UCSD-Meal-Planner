2b817b9bebbf32eda0b03e30f902bfc3015006d0
Ryan Le <ryanphle@gmail.com>
2017-09-28 18:13:10 -0700

    Changed settings and added Procfile and req file for deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x2i5qy^)3(gw1$uwqm*m-jd&8jcjozzv_ja=t&4rxt#n-*-$6k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ucsdmealplanner.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ucsdmealplanner.pythonanywhere.com', '127.0.0.1', '*']
 
 
 # Application definition
